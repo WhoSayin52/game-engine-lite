9aa92d50ec2e32713e4c48c31de663a37cd59454
WhoSayin52 <180821950+WhoSayin52@users.noreply.github.com>
2025-07-21 19:34:32 +0300

    Lua tables

--- assets/scripts/my_script.lua
+++ assets/scripts/my_script.lua
@@ -1,3 +1,13 @@
 --This is a lua comment: --
-
+--This is a lua var
 some_var = 9 * 10
+
+--This is a lua table 
+config = {
+	title = "My Game Engine",
+	fullscreen = false,
+	resolution = {
+		width = 800,
+		height = 600
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,28 @@
 
 #include <iostream>
 
-void test_lua(){
+void test_lua() {
 	sol::state lua;
-	
+
 	lua.open_libraries(sol::lib::base);
 
 	lua.script_file("../assets/scripts/my_script.lua");
 
-	int some_var_from_lua{lua["some_var"]};
-
+	//This is how we get vars from lua scripts
+	int some_var_from_lua{ lua["some_var"] };
 	std::cout << "The some var from lua " << some_var_from_lua << '\n';
+
+	//This is how we get values from lua table
+	bool is_full_screen = lua["config"]["fullscreen"];
+
+	std::cout << "We read full screen value is: " << is_full_screen << '\n';
+
+	//Or do this
+	sol::table config = lua["config"];
+	int width = config["resolution"]["width"];
+	int height = config["resolution"]["height"];
+
+	std::cout << "H: " << height << " | W: " << width << '\n';
 }
 
 int main() {
